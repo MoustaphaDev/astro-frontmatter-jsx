9613b82e4f6eef33a3b807954def74c41758c9a9
Moustapha HappyDev <moustapha.happydev@gmail.com>
2023-04-20 15:35:15 +0000

    chore: setup tailwind

--- demo/astro.config.mjs
+++ demo/astro.config.mjs
@@ -2,8 +2,9 @@ import { defineConfig } from 'astro/config';
 import astroFrontmatterJsx from 'astro-frontmatter-jsx';
 
 // https://astro.build/config
+import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-    integrations: [astroFrontmatterJsx()],
+    integrations: [tailwind(), astroFrontmatterJsx()],
 });
